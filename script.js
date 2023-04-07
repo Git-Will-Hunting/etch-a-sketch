c4aeb468e5a9c2f37b4c3323c6f9b6ce4ce1e32b
git-will-hunting <58862067+Git-Will-Hunting@users.noreply.github.com>
2023-04-07 04:49:24 -0400

    Add grid resizing feature

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
 const gridContainer = document.querySelector(".grid-container");
 const header = document.querySelector(".header");
-console.log(header);
-const gridSize = 16;
+const resetButton = document.querySelector("#reset-button");
+const gridSizeInput = document.querySelector(".grid-size-input");
+
 
 // get window size
 const windowWidth = window.innerWidth;
 const windowHeight = window.innerHeight - header.offsetHeight;
 
 // determine grid square size
-const gridSquareSize = Math.floor(Math.min(windowWidth, windowHeight) / gridSize);
+function getGridSquareSize() {
+    if (windowWidth < windowHeight) {
+        return Math.floor(windowWidth / gridSizeInput.value);
+    } else {
+        return Math.floor(windowHeight / gridSizeInput.value);
+    }
+}
 
-// set grid container size
-gridContainer.style.width = `${gridSquareSize * gridSize}px`;
-gridContainer.style.height = `${gridSquareSize * gridSize}px`;
+
+
 
 // create grid squares
-function drawGrid(gridSize) {
-    for (let i = 0; i < gridSize * gridSize; i++) {
+function drawGrid() {
+    const gridSquareSize = getGridSquareSize();
+    // set grid container size
+    gridContainer.style.width = `${gridSquareSize * gridSizeInput.value}px`;
+    gridContainer.style.height = `${gridSquareSize * gridSizeInput.value}px`;
+    for (let i = 0; i < (gridSizeInput.value * gridSizeInput.value); i++) {
         const gridSquare = document.createElement("div");
         gridSquare.classList.add("grid-square");
         gridSquare.style.width = `${gridSquareSize - 2}px`;
         gridSquare.style.height = `${gridSquareSize - 2}px`;
-        gridSquare.style.border = "1px solid black";
+        gridSquare.style.border = "1px rgba(0, 0, 0, 0.5) solid";
         gridContainer.appendChild(gridSquare);
     }
     // display grid squares with flex
     gridContainer.style.display = "flex";
     gridContainer.style.flexWrap = "wrap";
-    
+    // event listeners
+    const gridSquares = document.querySelectorAll(".grid-square");
+    gridSquares.forEach(gridSquare => {
+    gridSquare.addEventListener("mouseover", hoverEffect);
+});    
 }
 
 // grid square hover effect
@@ -36,15 +50,16 @@ function hoverEffect(e) {
     setTimeout(() => {
         e.target.style.backgroundColor = "white";
     }
-    , 1500);
+    , 100 * gridSizeInput.value);
 }
 
 
 // draw grid
-drawGrid(gridSize);
+drawGrid(gridSizeInput.value);
 
-// event listeners
-const gridSquares = document.querySelectorAll(".grid-square");
-gridSquares.forEach(gridSquare => {
-    gridSquare.addEventListener("mouseover", hoverEffect);
+
+
+resetButton.addEventListener("click", () => {
+    gridContainer.innerHTML = "";
+    drawGrid();
 });
